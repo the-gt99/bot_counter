cf937ab05f790a0293f293b4f1ebbdb1a530bfd4
the-gt99 <the_gt99@mail.ru>
2018-09-13 18:42:09 +0400

    sdsd

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const bodyParser = require('body-parser')
 const { Botact } = require('botact')
  
 const app = express()
+
 const bot = new Botact({
   confirmation: "d1527577",
   token: "af54bf0ee2a6051f81222b1058c88bbdb0ba217b9ecc399bd9c6c3af2f84dd0c1bd9b0a7b9a1af8388940"
 })
  
+bot.on(function (ctx){
+  console.log("test");
+  ctx.reply('123')
+})
+
 bot.command('start', ({ reply }) => {
   reply('This is start!')
 })
